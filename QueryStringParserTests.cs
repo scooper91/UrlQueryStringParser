253fb9a897499abdfa240a5c8d445773fe6cb530
Sophie Cooper <sophie.cooper@7digital.com>
2014-10-13 17:05:31 +0100

    Can ignore question marks and input a string to a dictionary

---
+++ QueryStringParserTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace QueryStringParser
+{
+	public class QueryStringParserTests
+	{
+		[Test]
+		public void Should_return_empty_list_when_empty_string_passed_in()
+		{
+			var parser = new UrlStringParser();
+			
+			Assert.That(parser.ParsedQueryString("").Count, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Should_return_empty_list_when_only_question_mark_entered()
+		{
+			var parser = new UrlStringParser();
+			
+			Assert.That(parser.ParsedQueryString("?").Count, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Should_return_key_value_if_key_entered()
+		{
+			var parser = new UrlStringParser();
+			var urlInput = parser.ParsedQueryString("test");
+			Assert.That(urlInput.ContainsKey("test"));
+			//Assert.That(urlInput.ContainsValue(null));
+			Assert.That(urlInput.Count, Is.EqualTo(1));
+		}
+	}
+}
---
+++ UrlStringParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace QueryStringParser
+{
+	public class UrlStringParser
+	{
+		public IDictionary<string,string> ParsedQueryString(string url)
+		{
+			var urlDictionary = new Dictionary<string, string>();
+
+			if (url == string.Empty || url.Contains("?"))
+			{
+				return urlDictionary;
+			}
+			
+			urlDictionary.Add(url, null);
+			return urlDictionary;
+		}
+	}
+}
